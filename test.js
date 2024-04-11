2e64942430056866302be34f92903c40e1af40bd
timmy-turner005 <timitalented005@gmail.com>
2024-04-11 14:24:20 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,41 @@
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  if (newItem.value === "") {
+    alert("please add an item sir/ma or whatever pronoun you go by");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+
+  itemInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+itemForm.addEventListener("submit", addItem);
